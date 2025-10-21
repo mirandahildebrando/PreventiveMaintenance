509c7fa647e3981228181330db9f1d1a0e29a7cb
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-10-21 14:50:26 -0300

    class service complete

--- src/main/java/com/hildebrando/ManutencaoPreventiva/repository/VehicleRepository.java
+++ src/main/java/com/hildebrando/ManutencaoPreventiva/repository/VehicleRepository.java
@@ -3,8 +3,10 @@ package com.hildebrando.ManutencaoPreventiva.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.hildebrando.ManutencaoPreventiva.model.Vehicle;
+
 @Repository
-public interface Vehicle extends JpaRepository<Vehicle, Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
 
 }
--- src/main/java/com/hildebrando/ManutencaoPreventiva/service/VehicleService.java
+++ src/main/java/com/hildebrando/ManutencaoPreventiva/service/VehicleService.java
@@ -1,5 +1,35 @@
 package com.hildebrando.ManutencaoPreventiva.service;
 
+import org.springframework.stereotype.Service;
+
+import com.hildebrando.ManutencaoPreventiva.model.MaintenancePlan;
+import com.hildebrando.ManutencaoPreventiva.model.Vehicle;
+import com.hildebrando.ManutencaoPreventiva.repository.VehicleRepository;
+
+@Service
 public class VehicleService {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+    
+        public Vehicle createVehicle (Vehicle vehicle) {
+            return vehicleRepository.save(vehicle);
+        }
+
+        public Vehicle getVehicleById(Long id) {
+            return vehicleRepository.findById(id).orElse(null);
+        }
+
+        public Double calculateNextReview(Long vehicleId) {
+            Vehicle vehicle = getVehicleById(vehicleId);
+
+            Double interval = ((MaintenancePlan) vehicle).calculateMaintenanceInterval();
+
+            return vehicle.getCurrentMileage() + interval;
+        }
+
 
 }
